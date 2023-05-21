785f1f3b8ea7c6d0de992eb021d95c74770cfeae
Scarlattea <scarlattea@gmail.com>
2023-05-21 22:05:57 +0200

    main page

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import { getDatabase } from "firebase/database";
-
 
 function LoginPage() {
     let onSubmitBtnClick = function(e){
-        console.log('submit clicked');
         console.log(e.target);
-        fetch('https://tcrw-hosting-default-rtdb.europe-west1.firebasedatabase.app/');
+    }
+    let vmi = function(){
+
+    }
+    function showRegister(){
+      document.querySelector('.login-modul').classList.add('d-none');
+      document.querySelector('.register-modul').classList.remove('d-none');
+    }
+    function showLogin(){
+      document.querySelector('.register-modul').classList.add('d-none');
+      document.querySelector('.login-modul').classList.remove('d-none');
     }
   return (
     <>
       <div>Welcome to TCRW hosting</div>
 
-      <button className='register'>Register</button>
-      <button className='login'>Log In</button>
+      <button className='register' onClick={showRegister}>Register</button>
+      <button className='login' onClick={showLogin}>Log In</button>
       
-      <div>Register</div>
-      <form>
-        <input className='username' type="text"></input>
-        <input className="password" type="password"></input>
-      </form>
-      <button className="register" onClick={onSubmitBtnClick}>submit</button>
-
-      <div>Log In</div>
-      <form>
-        <input className='username' type="text"></input>
-        <input className="password" type="password"></input>
-      </form>
-      <button className="login" onClick={onSubmitBtnClick}>submit</button>
+      <div className='register-modul d-none'>
+        <div>Register</div>
+        <form>
+          <input className='register username' type="text" onChange={vmi}></input>
+          <input className="register password" type="password"onChange={vmi}></input>
+        </form>
+        <button className="register submit" onClick={onSubmitBtnClick}>submit</button>
+      </div>
+      
+      <div className='login-modul d-none'>
+        <div>Log In</div>
+        <form>
+          <input className='login username' type="text" onChange={vmi}></input>
+          <input className="login password" type="password" onChange={vmi}></input>
+        </form>
+        <button className="login submit" onClick={onSubmitBtnClick}>submit</button>
+      </div>
+      
     </>
   );
 }
